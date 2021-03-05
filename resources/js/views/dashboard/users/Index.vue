4f1f78bb384f42dddeaec1b5afddd05f29ce385e
Neosoulink <neodarksoulink@gmail.com>
2021-03-05 16:24:43 +0100

    components initialization

---
+++ resources/js/views/dashboard/users/Index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="content">
+		<h2>Users</h2>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ resources/js/views/dashboard/users/Profile.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h2>Profile</h2>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
