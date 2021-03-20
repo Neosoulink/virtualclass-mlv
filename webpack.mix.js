bf925f6d0f06032f2faf63fc89060e052662a8c8
Neosoulink <neodarksoulink@gmail.com>
2021-03-20 16:57:50 +0100

    Fix laravel mix vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
     .sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync('virtualclass-mlv.dev');
