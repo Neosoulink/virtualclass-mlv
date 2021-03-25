0e61fb9c8c415eb58ca9bfa69dc2ba41110c3c11
Neosoulink <neodarksoulink@gmail.com>
2021-03-25 21:36:44 +0100

    Organization model + api resource controller

---
+++ app/Organization.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'description',
+		'owner'
+	];
+
+	/**
+	 * Organization users many to many ORM relationship.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function users()
+	{
+		return $this->belongsToMany('App\User', 'organization_users');
+	}
+}
