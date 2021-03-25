c437a6930ffdf3fdea595b8d779a78465c80b905
Neosoulink <neodarksoulink@gmail.com>
2021-03-25 02:43:37 +0100

    Mass asignment fillable fields

--- app/User.php
+++ app/User.php
@@ -20,7 +20,15 @@ class User extends Authenticatable implements JWTSubject
 	 * @var array
 	 */
 	protected $fillable = [
-		'name', 'email', 'password',
+		'name',
+		'email',
+		'password',
+		'phone_number',
+		'country',
+		'first_name',
+		'last_name',
+		'full_address',
+		'genre',
 	];
 
 	/**
