3f16c8dba97643087c2c068f8cdeb1af484cfc77
Neosoulink <neodarksoulink@gmail.com>
2021-03-25 01:58:24 +0100

    Add all seeders in database seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		$this->call(UserSeeder::class);
+		$this->call([
+			UserSeeder::class,
+			UserDocumentRoleSeeder::class,
+			OrganizationRoleSeeder::class,
+		]);
 	}
 }
