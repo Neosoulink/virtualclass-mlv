8631af99f48cbd4e4aa707751f1e2ef74753b537
Neosoulink <neodarksoulink@gmail.com>
2021-03-25 03:27:39 +0100

    Code review

--- database/migrations/2020_09_04_134418_create_documents_table.php
+++ database/migrations/2020_09_04_134418_create_documents_table.php
@@ -16,7 +16,7 @@ class CreateDocumentsTable extends Migration
 		Schema::create('documents', function (Blueprint $table) {
 			$table->id();
 			$table->string('name');
-			$table->name('config');
+			$table->string('config');
 			$table->timestamps();
 		});
 	}
