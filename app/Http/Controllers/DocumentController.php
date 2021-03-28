db2f48e898aeffe34c1322e3c8e9d8df50dde4d5
Neosoulink <neodarksoulink@gmail.com>
2021-03-28 19:35:55 +0100

    Api rousource mothods

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -25,7 +25,27 @@ class DocumentController extends Controller
 	 */
 	public function store(Request $request)
 	{
-		//
+		$validator = validator(
+			$request->all(),
+			[
+				"name" => "string|required",
+				"description" => "string|required",
+				"config" => "json|required",
+			]
+		);
+
+		if (!$validator->fails()) {
+			Document::create($validator->validate());
+			return response([
+				"data" =>  $validator->validate(),
+				"message" => "The document " . $validator->validate()["name"] . " has been created!"
+			]);
+		} else {
+			return response([
+				"data" =>  $validator->validate(),
+				"messages" => $validator->messages()
+			], 402);
+		}
 	}
 
 	/**
@@ -36,7 +56,7 @@ class DocumentController extends Controller
 	 */
 	public function show(Document $document)
 	{
-		//
+		return response()->json($document);
 	}
 
 	/**
@@ -48,7 +68,27 @@ class DocumentController extends Controller
 	 */
 	public function update(Request $request, Document $document)
 	{
-		//
+		$validator = validator(
+			$request->all(),
+			[
+				"name" => "string",
+				"description" => "string",
+				"config" => "json",
+			]
+		);
+
+		if (!$validator->fails()) {
+			$document->update($validator->validate());
+			return response([
+				"data" =>  $validator->validate(),
+				"message" => "The document " . $validator->validate()["name"] . " has been updated!"
+			]);
+		} else {
+			return response([
+				"data" =>  $validator->validate(),
+				"messages" => $validator->messages()
+			], 402);
+		}
 	}
 
 	/**
@@ -59,6 +99,12 @@ class DocumentController extends Controller
 	 */
 	public function destroy(Document $document)
 	{
-		//
+		$doc = $document;
+		$document->delete();
+
+		return response([
+			"data" =>  $doc,
+			"message" => "The document " . $doc->name . " has been deleted!"
+		]);
 	}
 }
